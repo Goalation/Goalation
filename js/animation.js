4fcfa29f9365f20a494703a72818f4e51a221789
tobydev-hub <80963549+tobydev-hub@users.noreply.github.com>
2021-03-30 12:23:24 +0200

    Add files via upload

--- js/animation.js
+++ js/animation.js
@@ -1,1 +1,18 @@
-test
+$(document).ready(function(){
+    $(".add-row").click(function(){
+        var period = $("#period").val();
+        var goal = $("#goal").val();
+        var donation = $("#donation").val();
+        var markup = "<tr><td><input type='checkbox' name='record'></td><td>" + period + "</td><td>" + goal + "</td><td>" + donation + "</td></tr>";
+        $("table tbody").append(markup);
+    });
+
+    // Find and remove selected table rows
+    $(".delete-row").click(function(){
+        $("table tbody").find('input[name="record"]').each(function(){
+            if($(this).is(":checked")){
+                $(this).parents("tr").remove();
+            }
+        });
+    });
+});
